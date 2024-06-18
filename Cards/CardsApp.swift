50d0e116f714daeb0abb268d2d772a360c7e9220
AlexandrBondar <bondar2014@gmail.com>
2024-06-18 15:03:43 +0300

    Initial commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by dev on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
